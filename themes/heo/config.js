6978b91581e9bb33b44b7b2ec75cbc1ca8bba200
Jiazan Wang <102038245+JiazanWang@users.noreply.github.com>
2024-09-29 22:03:57 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -21,11 +21,11 @@ const CONFIG = {
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: 'ZANTJU.COM',  
-  HEO_HERO_TITLE_3: 'ZANTJU.COM',    
-  HEO_HERO_TITLE_4: '记录我，遇见你！',                 
+  HEO_HERO_TITLE_2: '与思维认知',   
+  HEO_HERO_TITLE_3: 'JIAZANW.COM',    
+  HEO_HERO_TITLE_4: '记录我，遇见你！',                                  
   HEO_HERO_TITLE_5: '故事从这里开始。',
-  HEO_HERO_TITLE_LINK: 'https://zantju.com',  
+  HEO_HERO_TITLE_LINK: 'https://jiazanw.com',    
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
@@ -41,8 +41,8 @@ const CONFIG = {
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
     '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
+    '🤝 专修互联网与AI', 
+    '🏃 脚踏实地行动派',   
     '🏠 智能家居小能手',
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
@@ -51,9 +51,9 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://jiazanw.com/',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL3: 'https://github.com/JiazanWang',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -116,11 +116,11 @@ const CONFIG = {
     }
   ],
 
-  HEO_SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
+  HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
+  HEO_SOCIAL_CARD_TITLE_1: '交流频道',  
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://jiazanw.com/',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
